9f2dc84bc9f8fc6536bd085861cd5a2eb8946f92
jazzley <jate1601@student.miun.se>
2024-04-10 19:07:32 +0200

    Maze generation as well as point setting done.

---
+++ cmake-build-debug/CMakeFiles/objekt_projekt.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Dodo/CLionProjects/Objekt_Projekt/Maze/maze.cpp" "CMakeFiles/objekt_projekt.dir/Maze/maze.cpp.obj" "gcc" "CMakeFiles/objekt_projekt.dir/Maze/maze.cpp.obj.d"
+  "C:/Users/Dodo/CLionProjects/Objekt_Projekt/Support/findWidthAndHeight.cpp" "CMakeFiles/objekt_projekt.dir/Support/findWidthAndHeight.cpp.obj" "gcc" "CMakeFiles/objekt_projekt.dir/Support/findWidthAndHeight.cpp.obj.d"
+  "C:/Users/Dodo/CLionProjects/Objekt_Projekt/main.cpp" "CMakeFiles/objekt_projekt.dir/main.cpp.obj" "gcc" "CMakeFiles/objekt_projekt.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/objekt_projekt.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/objekt_projekt.dir/Maze/maze.cpp.obj"
+  "CMakeFiles/objekt_projekt.dir/Maze/maze.cpp.obj.d"
+  "CMakeFiles/objekt_projekt.dir/Support/findWidthAndHeight.cpp.obj"
+  "CMakeFiles/objekt_projekt.dir/Support/findWidthAndHeight.cpp.obj.d"
+  "CMakeFiles/objekt_projekt.dir/main.cpp.obj"
+  "CMakeFiles/objekt_projekt.dir/main.cpp.obj.d"
+  "libobjekt_projekt.dll.a"
+  "objekt_projekt.exe"
+  "objekt_projekt.exe.manifest"
+  "objekt_projekt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/objekt_projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
